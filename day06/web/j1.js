93b1d10fd5d71b6fd18f09075c90d842c5925870
HyeBeen <31886887+saltandlight@users.noreply.github.com>
2019-06-18 18:32:42 +0900

    Add files via upload

---
+++ day06/web/j1.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+var a;
+var A;
+
+/*alert('1');
+alert('2');
+alert('3');
+
+setInterval(function(){
+	var now = new Date();
+	document.write(now); body와 /body 사이에 now를 뿌려라~~
+	
+	
+}, 1000);*/
+/*이 함수 안에 시간과 함수를 넣어줌, 이름이 없는 함수=ananymous function
+ * 1초에 한 번씩 함수가 움직임.
+ */
+ 
